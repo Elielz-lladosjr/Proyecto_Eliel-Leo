4ae9f04c7cc41893e01aa1a269204b5b29002a70
Leo <leo@localhost.localdomain>
2025-11-21 12:40:19 +0100

    Avanzes header

--- mercadona-clone/src/components/Navbar.tsx
+++ mercadona-clone/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Search, User, ShoppingCart, Menu } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import styles from './Navbar.module.css'; // Importamos el CSS Module
 
 const Navbar = () => {
@@ -10,31 +10,7 @@ const Navbar = () => {
           <button className={styles.menuBtn}>
             <Menu size={24} />
           </button>
-          <div className={styles.logo}>Mercadona</div>
-        </div>
-
-        {/* Buscador */}
-        <div className={styles.searchSection}>
-          <div className={styles.searchWrapper}>
-            <input 
-              type="text" 
-              placeholder="Buscar productos..." 
-              className={styles.searchInput}
-            />
-            <Search className={styles.searchIcon} size={20} />
-          </div>
-        </div>
-
-        {/* Iconos Derecha */}
-        <div className={styles.actions}>
-          <button className={styles.actionBtn}>
-            <User size={22} />
-            <span className={styles.userText}>Hola, Identifícate</span>
-          </button>
-          <button className={styles.cartBtn}>
-            <ShoppingCart size={24} />
-            <span className={styles.badge}>0</span>
-          </button>
+          <img src='/logoMercadona.svg' className={styles.logo}/>
         </div>
       </div>
     </nav>
