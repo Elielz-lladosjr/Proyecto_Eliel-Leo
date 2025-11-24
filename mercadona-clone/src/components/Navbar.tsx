883316c4416446916430c4bbc4bb82568b3416f4
Leo <leo@localhost.localdomain>
2025-11-24 12:28:20 +0100

    Header terminado

--- mercadona-clone/src/components/Navbar.tsx
+++ mercadona-clone/src/components/Navbar.tsx
@@ -1,19 +1,37 @@
+import { Navbar as BSNavbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { Menu } from 'lucide-react';
-import styles from './Navbar.module.css'; // Importamos el CSS Module
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.container}>
-        {/* Logo y Menú */}
-        <div className={styles.logoSection}>
-          <button className={styles.menuBtn}>
-            <Menu size={24} />
-          </button>
-          <img src='/logoMercadona.svg' className={styles.logo}/>
-        </div>
-      </div>
-    </nav>
+    <BSNavbar expand="lg" className={styles.navbar}>
+      <Container fluid className={styles.container}>
+        <BSNavbar.Toggle aria-controls="basic-navbar-nav" className={styles.menuBtn}>
+          <Menu size={24} />
+        </BSNavbar.Toggle>
+        
+        <BSNavbar.Brand href="#home" className={styles.brand}>
+          <img src='/logoMercadona.svg' className={styles.logo} alt="Mercadona Logo" />
+        </BSNavbar.Brand>
+
+        <BSNavbar.Collapse id="basic-navbar-nav">
+          <Nav className={`me-auto ${styles.navLinks}`}>
+            <Nav.Link href="#conocenos" className={styles.navLink}>Conócenos</Nav.Link>
+            <Nav.Link href="#supermercados" className={styles.navLink}>Supermercados</Nav.Link>
+            <Nav.Link href="#trabaja" className={styles.navLink}>Trabaja con nosotros</Nav.Link>
+            <Nav.Link href="#atencion" className={styles.navLink}>Atención al cliente</Nav.Link>
+          </Nav>
+          
+          <Nav className={styles.langSelectorWrapper}>
+            <NavDropdown title="Español" id="basic-nav-dropdown" align="end" className={styles.langSelector}>
+              <NavDropdown.Item href="#es">Español</NavDropdown.Item>
+              <NavDropdown.Item href="#en">English</NavDropdown.Item>
+              <NavDropdown.Item href="#pt">Português</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </BSNavbar.Collapse>
+      </Container>
+    </BSNavbar>
   );
 };
 
